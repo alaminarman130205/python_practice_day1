4c64d51cf2a7fd9fe58ad3c7d45286d467614776
Alamin Arman <108583030+alaminarman130205@users.noreply.github.com>
2023-05-07 02:33:41 +0600

    Add files via upload
    arithmetic operation

---
+++ day1.py
@@ -0,0 +1,40 @@
+x = 12
+y = 10
+
+# Arithmetic operation step by step 
+# addition 
+print("addition")
+
+a = x+y
+print(a)
+
+
+#subtraction 
+print("subtraction")
+
+
+s = x-y
+print(s)
+
+#multiplication 
+print("multiplication")
+
+m = x*y 
+print(m)
+
+#division 
+print("division")
+d = x/y
+print(d)
+
+#modulus 
+print('modulus')
+mo = x%y
+print(mo)
+
+#exponentiation 
+ex = x**y
+print(ex)
+#floor division
+fd = x//y
+print(fd)
